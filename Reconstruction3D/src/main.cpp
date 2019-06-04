9c887a1465709c4414eb9052eee07cad09320f19
Felipe Duque Belfort de Oliveira <fdbo@up1c05.windows.cin.ufpe.br>
2019-06-04 08:04:50 -0300

    first commit

---
+++ Reconstruction3D/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 4 de jun de 2019
+ *      Author: fdbo
+ */
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, fellow humans" << std::endl;
+
+	return 0;
+}
+
+
+
+
