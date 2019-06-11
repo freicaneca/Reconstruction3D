c678c89fbc7ca6b70670f980efdef8ff2d466186
Felipe Duque Belfort de Oliveira <fdbo@up1c05.windows.cin.ufpe.br>
2019-06-11 08:22:07 -0300

    test environment

--- Reconstruction3D/Debug/src/subdir.mk
+++ Reconstruction3D/Debug/src/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/media/fdbo/seagate_expansion/doutorado/faculdade/topicos_midia_interacao_1/projeto/eclipse/Reconstruction3D/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
